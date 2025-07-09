958088458cc408793c20a886831fcfe31c3c4624
Kadjow <132962594+Kadjow@users.noreply.github.com>
2025-07-09 12:32:08 -0300

    feat: added the strambuilder in main

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:habit_tracker/features/habit_tracker/auth/presentation/pages/login_page.dart';
 import 'package:habit_tracker/features/habit_tracker/presentation/pages/homePage.dart';
-import 'firebase_options.dart';                        
-import 'injection_container.dart';
+
+import 'firebase_options.dart';               
+import 'injection_container.dart';            
 import 'core/services/auth_service.dart';
 import 'features/habit_tracker/presentation/bloc/habit/habit_bloc.dart';
 import 'features/habit_tracker/presentation/bloc/habit/habit_event.dart';
@@ -33,21 +34,30 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Habit Tracker',
         theme: ThemeData.dark(),
-        home: StreamBuilder<User?>(
-          stream: sl<AuthService>().authStateChanges,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Scaffold(
-                body: Center(child: CircularProgressIndicator()),
-              );
-            }
-            if (snapshot.hasData) {
-              return const HomePage();
-            }
-            return LoginPage();
-          },
-        ),
+        home: const AuthGate(),
       ),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: sl<AuthService>().authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(body: Center(
+            child: CircularProgressIndicator(),
+          ));
+        }
+        if (snapshot.hasData) {
+          return const HomePage();
+        }
+        return const LoginPage();
+      },
     );
   }
 }
